4e396ea38f63156f880561d273cc2aac50b64ced
Cheng (Caspar) Chen <susume@connect.hku.hk>
2019-10-01 23:48:50 -0700

    added several solutions for leetcode problems

--- C++/minimumHeightTrees.cpp
+++ C++/minimumHeightTrees.cpp
@@ -1,37 +1,41 @@
 class Solution {
 public:
-    vector<int> findMinHeightTrees(int n, vector<pair<int, int>>& edges) {
-        bool* visited = new bool[n];
-        int curmin = n, maxcost = 0;
-        vector<int> results;
-        for (int i=0; i<n; i++){
-            for (int j=0; j<n; j++){
-                visited[j] = false;
-            }
-            maxcost = 0;
-            dfs(i,edges,visited,0, maxcost,0,n);
-            cout << maxcost << "\t";
-            results.push_back(maxcost);
-            if (maxcost<curmin) curmin=maxcost;
+    vector<int> findMinHeightTrees(int n, vector<vector<int>>& edges) {
+        vector<int> res;
+        if(n == 1){
+            res = {0};
+            return res;
         }
-        vector<int> sol;
-        for (int i=0; i<n; i++){
-            if (results[i]==curmin) sol.push_back(i);
+        vector<int> degree(n);
+        vector<vector<int>> adj(n);
+        for(auto& edge: edges){
+            adj[edge[0]].push_back(edge[1]);
+            adj[edge[1]].push_back(edge[0]);
+            degree[edge[0]] ++;
+            degree[edge[1]] ++;
         }
-        return sol;
-    }
-    
-    void dfs(int tovisit, vector<pair<int, int>>& edges, bool* visited, int cost, int& maxcost, int numvisited, int n){
-        visited[tovisit] = true; numvisited++;
-        if (cost>maxcost) maxcost=cost;
-        if (numvisited==n) return;
-        for (auto it=edges.begin(); it!=edges.end(); it++){
-            if (it->second == tovisit && !visited[it->first]){
-                dfs(it->first, edges, visited, cost+1, maxcost, numvisited, n);
-            }
-            else if (it->first == tovisit && !visited[it->second]){
-                dfs(it->second, edges, visited, cost+1, maxcost, numvisited, n);
+        
+        queue<int> myq, nextq;
+        for(int i = 0; i < n; ++ i){
+            if(degree[i] == 1){
+                myq.push(i);
             }
         }
+        
+        while(!myq.empty()){
+            res.clear();
+            auto size = myq.size();
+            for(auto i = 0; i < size; ++ i){
+                int cur = myq.front();
+                myq.pop();
+                res.push_back(cur);
+                for(int j: adj[cur]){
+                    degree[j] --;
+                    if(degree[j] == 1)
+                        myq.push(j);
+                }
+            }
+        }
+        return res;
     }
 };
